95f432b1b70c3d6d7aef881d2dad87146a8eb88b
1715924005 <470659548@qq.com>
2019-09-16 16:01:31 +0800

    上传项目

---
+++ src/main/java/com/longcheng/xxh/energycenter/EnergycenterApplication.java
@@ -0,0 +1,13 @@
+package com.longcheng.xxh.energycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergycenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergycenterApplication.class, args);
+    }
+
+}
