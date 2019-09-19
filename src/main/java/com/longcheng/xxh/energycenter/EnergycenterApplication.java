bd2a51a6f39257ba811bd50c4f76eb28bede88e0
zimengli <2860919435@qq.com>
2019-09-19 14:58:19 +0800

    数据导入

--- src/main/java/com/longcheng/xxh/energycenter/EnergycenterApplication.java
+++ src/main/java/com/longcheng/xxh/energycenter/EnergycenterApplication.java
@@ -1,13 +1,26 @@
 package com.longcheng.xxh.energycenter;
 
+import com.longcheng.xxh.energycenter.excel.excelToOracle;
+import jxl.read.biff.BiffException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class EnergycenterApplication {
 
     public static void main(String[] args) {
+        excelToOracle in = new excelToOracle();
+        try {
+            in.insert("C:/Users/zml/Desktop/报表项目筛选.xls","EMS_GAS_POINTCOLLECTION");
+        } catch (BiffException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(EnergycenterApplication.class, args);
     }
+
 
 }
