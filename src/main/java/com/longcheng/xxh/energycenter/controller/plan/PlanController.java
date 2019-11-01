f816cc7d4c9eb84733d350a1768bc1c01840965e
xieqi <947962606@qq.com>
2019-11-01 16:32:56 +0800

    接口跨域问题，contorller层添加注解

--- src/main/java/com/longcheng/xxh/energycenter/controller/plan/PlanController.java
+++ src/main/java/com/longcheng/xxh/energycenter/controller/plan/PlanController.java
@@ -5,9 +5,8 @@ import com.longcheng.xxh.energycenter.entity.basepo.Code;
 import com.longcheng.xxh.energycenter.entity.basepo.Results;
 import com.longcheng.xxh.energycenter.entity.plan.Plan;
 import com.longcheng.xxh.energycenter.service.plan.PlanService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
@@ -20,8 +19,8 @@ import java.util.Map;
  * @author xieqi
  * @date 2019/10/18
  */
-@RestController
-@RequestMapping(value = "/plan")
+@Controller
+@RequestMapping(value = "/plan",method = RequestMethod.POST)
 public class PlanController {
 
     @Resource
@@ -32,15 +31,14 @@ public class PlanController {
      * @author xieqi
      * @date 2019/10/18
      **/
-    @RequestMapping("/add")
-    public String add(Plan plan){
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value ="/add",method = RequestMethod.POST)
+    public Results add(Plan plan){
         Date date = new Date();
         SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd :hh:mm:ss");
         plan.setCreateBy("admin");
         plan.setCreateDate(dateFormat.format(date));
-        planService.add(plan);
-        Results results = new Results(Code.success, "添加计划成功",plan, "添加计划内容");
-        return JSON.toJSONString(results);
+        return  planService.add(plan);
     }
 
     /**
@@ -48,11 +46,10 @@ public class PlanController {
      * @author xieqi
      * @date 2019/10/18
      **/
-    @RequestMapping("/delete")
-    public String delete(String id){
-        Results result =planService.delete(id);
-        Results results =  new Results(Code.success, "修改计划成功",result, "修改计划内容");
-        return JSON.toJSONString(results);
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value ="/delete",method = RequestMethod.POST)
+    public Results delete(int id){ return planService.delete(id);
+
     }
 
     /**
@@ -60,11 +57,10 @@ public class PlanController {
      * @author xieqi
      * @date 2019/10/18
      **/
-    @RequestMapping("/update")
-    public String update(Plan plan){
-        planService.update(plan);
-        Results results = new Results(Code.success, "修改计划成功",plan, "修改计划内容");
-        return JSON.toJSONString(results);
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value ="/update",method = RequestMethod.POST)
+    public Results update(Plan plan){ return  planService.update(plan);
+
     }
 
     /**
@@ -73,18 +69,12 @@ public class PlanController {
      * @date 2019/10/18
      *
      * @return*/
-
-    @RequestMapping("/findById")
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value ="/findById",method = RequestMethod.POST)
     public String findById(int id){
         Plan plan = planService.findById(id);
-        if(plan !=null ){
-            Results results = new Results(Code.success, "根据ID查询计划内容成功", plan, "通过ID查询计划内容");
-            return JSON.toJSONString(results);
-        }else {
-            Results results = new Results(Code.error, "根据ID查询计划内容失败", null, "通过ID查询计划内容");
-            return JSON.toJSONString(results);
-            }
-
+        Results results = new Results(Code.success, "根据ID查询计划内容成功", plan, "通过ID查询计划内容");
+        return JSON.toJSONString(results);
     }
 
 
@@ -93,7 +83,8 @@ public class PlanController {
      * @author xieqi
      * @date 2019/10/18
      **/
-    @RequestMapping("/pageList")
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value ="/pageList",method = RequestMethod.POST)
     public Map<String, Object> pageList(@RequestParam(required = false, defaultValue = "0") int offset,
                                         @RequestParam(required = false, defaultValue = "20") int pagesize) {
         return planService.pageList(offset, pagesize);
@@ -105,14 +96,15 @@ public class PlanController {
      * @author xieqi
      * @date 2019/10/18
      **/
-    @RequestMapping("/findByArea")
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value ="/findByArea",method = RequestMethod.POST)
     public String findByArea(String area){
         List<Plan> plan =planService.findByArea(area);
         if(area != null && area != ""){
             Results results = new Results(Code.success, "查询计划内容成功", plan, "通过区域查询计划内容");
             return JSON.toJSONString(results);
         }else{
-            Results results = new Results(Code.error, "查询计划内容失败", null, "通过区域查询计划内容");
+            Results results = new Results(Code.success, "查询计划内容失败", null, "通过区域查询计划内容");
             return JSON.toJSONString(results);
         }
     }
@@ -124,14 +116,15 @@ public class PlanController {
      * @author xieqi
      * @date 2019/10/18
      **/
-    @RequestMapping("/findByMediaName")
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value ="/findByMediaName",method = RequestMethod.POST)
     public String findByMediaName(String mediaName){
         List<Plan> plan =planService.findByMediaName(mediaName);
         if(mediaName != null && mediaName != ""){
             Results results = new Results(Code.success, "查询计划内容成功",plan, "通过介质名称查询计划内容");
             return JSON.toJSONString(results);
         }else{
-            Results results = new Results(Code.error, "查询计划内容失败", null, "通过介质名称查询计划内容");
+            Results results = new Results(Code.success, "查询计划内容失败", null, "通过介质名称查询计划内容");
             return JSON.toJSONString(results);
         }
 
@@ -143,7 +136,8 @@ public class PlanController {
      * @author xieqi
      * @date 2019/10/18
      **/
-    @RequestMapping("/findAll")
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value ="/findAll",method = RequestMethod.POST)
     public String findAll(){
         List<Plan> plan =planService.findAll();
         Results results = new Results(Code.success, "根据介质类型查询计划内容", plan, "根据介质类型查询计划内容");
