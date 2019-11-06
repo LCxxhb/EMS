cf94cbe3439d9f31cc6a9c6b06f097c8e43a0da6
1715924005 <470659548@qq.com>
2019-11-06 08:39:10 +0800

    Merge branch 'master' of D:\GitPro\EMS with conflicts.

---
+++ src/main/java/com/longcheng/xxh/energycenter/entity/mass/Patch.java
@@ -0,0 +1,121 @@
+package com.longcheng.xxh.energycenter.entity.mass;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author 
+ */
+public class Patch implements Serializable {
+    private BigDecimal id;
+
+    private String patchName;
+
+    private String createDate;
+
+    private String createBy;
+
+    private String spare1;
+
+    private String spare2;
+
+    private static final long serialVersionUID = 1L;
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getPatchName() {
+        return patchName;
+    }
+
+    public void setPatchName(String patchName) {
+        this.patchName = patchName;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getSpare1() {
+        return spare1;
+    }
+
+    public void setSpare1(String spare1) {
+        this.spare1 = spare1;
+    }
+
+    public String getSpare2() {
+        return spare2;
+    }
+
+    public void setSpare2(String spare2) {
+        this.spare2 = spare2;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Patch other = (Patch) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getPatchName() == null ? other.getPatchName() == null : this.getPatchName().equals(other.getPatchName()))
+            && (this.getCreateDate() == null ? other.getCreateDate() == null : this.getCreateDate().equals(other.getCreateDate()))
+            && (this.getCreateBy() == null ? other.getCreateBy() == null : this.getCreateBy().equals(other.getCreateBy()))
+            && (this.getSpare1() == null ? other.getSpare1() == null : this.getSpare1().equals(other.getSpare1()))
+            && (this.getSpare2() == null ? other.getSpare2() == null : this.getSpare2().equals(other.getSpare2()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getPatchName() == null) ? 0 : getPatchName().hashCode());
+        result = prime * result + ((getCreateDate() == null) ? 0 : getCreateDate().hashCode());
+        result = prime * result + ((getCreateBy() == null) ? 0 : getCreateBy().hashCode());
+        result = prime * result + ((getSpare1() == null) ? 0 : getSpare1().hashCode());
+        result = prime * result + ((getSpare2() == null) ? 0 : getSpare2().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", patchName=").append(patchName);
+        sb.append(", createDate=").append(createDate);
+        sb.append(", createBy=").append(createBy);
+        sb.append(", spare1=").append(spare1);
+        sb.append(", spare2=").append(spare2);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
